006b220f16c7a227eee379fb6d971366893ffbde
MobKerney <jason.kerney@hunterindustries.com>
2018-02-02 09:51:10 -0800

    Better expected type from response

--- signetBuilder.js
+++ signetBuilder.js
@@ -1,17 +1,20 @@
 const signet = require('signet')();
 
-signet.alias('response', '*');
 signet.alias('route', 'string');
 signet.alias('path', 'string');
 signet.alias('contentType', 'string');
 signet.alias('routerCallback', 'function<* => undefined>');
 signet.alias('routeNotFoundCallback', 'function<*, string => undefined>');
 
-
 signet.defineDuckType('commandLineArgument', {
     value: '*',
     valid: 'boolean',
     isError: 'boolean'
+});
+
+signet.defineDuckType('response', {
+    writeHead: 'function',
+    end: 'function'
 });
 
 module.exports = signet;
